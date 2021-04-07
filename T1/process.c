fd847ea229a7588c42bc08e26ae228447853dff3
diegomendieta <damendieta@LAPTOP-PBM71SBR.localdomain>
2021-04-07 18:38:00 -0400

    Worker funcionando sin las interrupciones ni output de salida.

--- T1/process.c
+++ T1/process.c
@@ -1,7 +1,8 @@
-#include <sys/types.h>
-// #include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "process.h"
 
@@ -11,16 +12,18 @@ void execWorker(char** start_line){
 
     int status;
 
-    char* exec = start_line[1];
+    char* to_exec = start_line[1];
     const int n = atoi(start_line[2]);
 
-    char* args[n];
-    for (int counter = 0; counter < n; counter++){
-        args[counter] = start_line[3 + counter];
+    char* args[n+2];
+    args[0] = to_exec;
+    for (int counter = 1; counter <= n; counter++){
+        args[counter] = start_line[3 + (counter - 1)];
         printf("arg[%d]: %s\n", counter, args[counter]);
     }
+    args[n+1] = NULL;
 
-    printf("Exec: %s\n", exec);
+    printf("Exec: %s\n", to_exec);
     printf("n: %d\n", n);
 
     pid_t child_pid = fork();
@@ -28,13 +31,17 @@ void execWorker(char** start_line){
     // Proceso hijo.
     if (child_pid == 0){
         printf("Ejecutando el proceso hijo...");
-        execve(exec);
+        execvp(args[0], args);
+        printf("Esta línea no debería mostrarse.");
+        exit(0);
     }
 
     // Proceso padre.
     else if (child_pid > 0){
-        printf("Ejecutando el proceso padre...");
+        printf("Ejecutando el proceso padre...\n");
         wait(&status);
+        printf("Código de salida del proceso hijo: %d\n", WEXITSTATUS(status));
+        exit(0);
     }
 }
 
